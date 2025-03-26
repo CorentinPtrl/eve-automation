668c9addac0bd0b5b96e95b775c61ef757b31d5a
lucille452 <lucille@cenacfamily.com>
2025-03-26 19:04:54 +0000

    feat(terraform): startup config

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,17 +1,17 @@
 locals {
   routers = {
-    R1 = { top = 50, left = 420, config = "hostname R1" }
-    R2 = { top = 145, left = 340, config = "hostname R2" }
-    R3 = { top = 145, left = 510, config = "hostname R3" }
-    R4 = { top = 50, left = 730, config = "hostname R4" }
+    R1 = { top = 50, left = 420, hostname = "hostname R1", ip = "192.168.100.45" }
+    R2 = { top = 145, left = 340, hostname = "hostname R2", ip = "192.168.100.46" }
+    R3 = { top = 145, left = 510, hostname = "hostname R3", ip = "192.168.100.47" }
+    R4 = { top = 50, left = 730, hostname = "hostname R4", ip = "192.168.100.48" }
   }
 
   switchs = {
-    SW1 = { top = 310, left = 340, config = "hostname SW1" }
-    SW2 = { top = 310, left = 510, config = "hostname SW2" }
-    SW3 = { top = 440, left = 420, config = "hostname SW3" }
-    SW4 = { top = 220, left = 730, config = "hostname SW4" }
-    OOB = { top = 120, left = 120, config = "hostname OOB" }
+    SW1 = { top = 310, left = 340, hostname = "hostname SW1", ip = "192.168.100.40" }
+    SW2 = { top = 310, left = 510, hostname = "hostname SW2", ip = "192.168.100.41" }
+    SW3 = { top = 440, left = 420, hostname = "hostname SW3", ip = "192.168.100.42" }
+    SW4 = { top = 220, left = 730, hostname = "hostname SW4", ip = "192.168.100.43" }
+    OOB = { top = 120, left = 120, hostname = "hostname OOB", ip = "192.168.100.44" }
 
   }
 
@@ -24,14 +24,14 @@ locals {
   connections = [
     { source = "R1", source_port = "Gi0/0", target = "R4", target_port = "Gi0/0" },
     { source = "R1", source_port = "Gi0/1", target = "R2", target_port = "Gi0/0" },
-    { source = "R1", source_port = "Gi0/2", target = "R3", target_port = "Gi0/0" },
+    { source = "R1", source_port = "Gi0/3", target = "R3", target_port = "Gi0/0" },
     { source = "R2", source_port = "Gi0/1", target = "SW1", target_port = "Gi0/0" },
     { source = "R3", source_port = "Gi0/1", target = "SW2", target_port = "Gi0/0" },
     { source = "R4", source_port = "Gi0/1", target = "SW4", target_port = "Gi0/0" },
     { source = "SW1", source_port = "Gi0/1", target = "SW3", target_port = "Gi0/0" },
     { source = "SW2", source_port = "Gi0/1", target = "SW3", target_port = "Gi0/1" },
     { source = "SW4", source_port = "Gi0/1", target = "DHCP", target_port = "eth0" },
-    { source = "OOB", source_port = "Gi0/0", target = "R1", target_port = "Gi0/3" },
+    { source = "OOB", source_port = "Gi0/0", target = "R1", target_port = "Gi0/2" },
     { source = "OOB", source_port = "Gi0/1", target = "R2", target_port = "Gi0/2" },
     { source = "OOB", source_port = "Gi0/2", target = "R3", target_port = "Gi0/2" },
     { source = "OOB", source_port = "Gi0/3", target = "R4", target_port = "Gi0/2" },
