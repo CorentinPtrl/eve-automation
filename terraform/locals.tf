641fa27836f4bcc694aa6745cf8e6124e9e1db45
lucille452 <lucille@cenacfamily.com>
2025-03-30 16:29:57 +0000

    feat(terraform): hostname

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -5,36 +5,33 @@ locals {
     FR_TLS_CORE2 = { top = 145, left = 510, hostname = "hostname FR_TLS_CORE2", ip = "192.168.100.47" }
     EN_FLT_R1 = { top = 50, left = 730, hostname = "hostname EN_FLT_R1", ip = "192.168.100.48" }
   }
-
   switchs = {
     FR_TLS_DIST1 = { top = 310, left = 340, hostname = "hostname FR_TLS_DIST1", ip = "192.168.100.40" }
     FR_TLS_DIST2 = { top = 310, left = 510, hostname = "hostname FR_TLS_DIST2", ip = "192.168.100.41" }
     FR_TLS_ACC = { top = 440, left = 420, hostname = "hostname FR_TLS_ACC", ip = "192.168.100.42" }
     EN_FLT_SW1 = { top = 220, left = 730, hostname = "hostname EN_FLT_SW1", ip = "192.168.100.43" }
   }
-
   cloud = {
     OOB = { top = 30, left = 30, type = "pnet0" }
     EN_FLT_SRV-DHCP = { top = 330, left = 660, type = "pnet1" }
   }
-
   connections = [
-    { source = "R1", source_port = "Gi0/0", target = "R4", target_port = "Gi0/0" },
-    { source = "R1", source_port = "Gi0/1", target = "R2", target_port = "Gi0/0" },
-    { source = "R1", source_port = "Gi0/3", target = "R3", target_port = "Gi0/0" },
-    { source = "R2", source_port = "Gi0/1", target = "SW1", target_port = "Gi0/0" },
-    { source = "R3", source_port = "Gi0/1", target = "SW2", target_port = "Gi0/0" },
-    { source = "R4", source_port = "Gi0/1", target = "SW4", target_port = "Gi0/0" },
-    { source = "SW1", source_port = "Gi0/1", target = "SW3", target_port = "Gi0/0" },
-    { source = "SW2", source_port = "Gi0/1", target = "SW3", target_port = "Gi0/1" },
-    { source = "SW4", source_port = "Gi0/1", target = "EN_FLT_SRV-DHCP", target_port = "eth0"},
-    { source = "R1", source_port = "Gi0/2", target = "OOB"},
-    { source = "R2", source_port = "Gi0/2", target = "OOB"},
-    { source = "R3", source_port = "Gi0/2", target = "OOB"},
-    { source = "R4", source_port = "Gi0/2", target = "OOB"},
-    { source = "SW1", source_port = "Gi0/2", target = "OOB"},
-    { source = "SW2", source_port = "Gi0/2", target = "OOB"},
-    { source = "SW3", source_port = "Gi0/2", target = "OOB"},
-    { source = "SW4", source_port = "Gi0/2", target = "OOB"}
+    { source = "FR_TLS_R1", source_port = "Gi0/0", target = "EN_FLT_R1", target_port = "Gi0/0" },
+    { source = "FR_TLS_R1", source_port = "Gi0/1", target = "FR_TLS_CORE1", target_port = "Gi0/0" },
+    { source = "FR_TLS_R1", source_port = "Gi0/3", target = "FR_TLS_CORE2", target_port = "Gi0/0" },
+    { source = "FR_TLS_CORE1", source_port = "Gi0/1", target = "FR_TLS_DIST1", target_port = "Gi0/0" },
+    { source = "FR_TLS_CORE2", source_port = "Gi0/1", target = "FR_TLS_DIST2", target_port = "Gi0/0" },
+    { source = "EN_FLT_R1", source_port = "Gi0/1", target = "EN_FLT_SW1", target_port = "Gi0/0" },
+    { source = "FR_TLS_DIST1", source_port = "Gi0/1", target = "FR_TLS_ACC", target_port = "Gi0/0" },
+    { source = "FR_TLS_DIST2", source_port = "Gi0/1", target = "FR_TLS_ACC", target_port = "Gi0/1" },
+    { source = "EN_FLT_SW1", source_port = "Gi0/1", target = "EN_FLT_SRV-DHCP", target_port = "eth0"},
+    { source = "FR_TLS_R1", source_port = "Gi0/2", target = "OOB"},
+    { source = "FR_TLS_CORE1", source_port = "Gi0/2", target = "OOB"},
+    { source = "FR_TLS_CORE2", source_port = "Gi0/2", target = "OOB"},
+    { source = "EN_FLT_R1", source_port = "Gi0/2", target = "OOB"},
+    { source = "FR_TLS_DIST1", source_port = "Gi0/2", target = "OOB"},
+    { source = "FR_TLS_DIST2", source_port = "Gi0/2", target = "OOB"},
+    { source = "FR_TLS_ACC", source_port = "Gi0/2", target = "OOB"},
+    { source = "EN_FLT_SW1", source_port = "Gi0/2", target = "OOB"}
   ]
 }
